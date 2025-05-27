cecb3683231157a5091f68a457bd527c4ff2fa5a
Farouk Nabeel <farouknabil810@gmail.com>
2025-05-27 21:25:09 +0200

    fetch personal blogs for each admin

--- components/UserBlogsList.tsx
+++ components/UserBlogsList.tsx
@@ -1,12 +1,18 @@
 import prisma from "@/lib/prisma";
 import React from "react";
 import UserBlogs from "./UserBlogs";
+import { auth } from "@/auth";
 
 export const experimental_ppr = true;
 const UserBlogsList = async () => {
+  const session = await auth();
+  const userId = session?.user.id;
   const posts = await prisma.post.findMany({
     orderBy: {
       createdAt: "desc",
+    },
+    where: {
+      authorId: userId,
     },
     include: {
       categories: {
